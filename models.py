38d976878e3f1981f708090ce6d76c7018597b81
nt1983 <73156205+nt1983@users.noreply.github.com>
2021-04-07 17:47:42 -0700

    update for Heroku

--- models.py
+++ models.py
@@ -1,12 +1,11 @@
 def create_classes(db):
-    class Pet(db.Model):
-        __tablename__ = 'pets'
+    class Website_Analytics(db.Model):
+        __tablename__ = 'Nov_devices'
 
-        id = db.Column(db.Integer, primary_key=True)
-        name = db.Column(db.String(64))
-        lat = db.Column(db.Float)
-        lon = db.Column(db.Float)
-
+        Device_Name = db.Column(db.String(10), primary_key=True)
+        Device_Count = db.Column(db.integer)
+        Device_Percentage = db.Column(db.Float)
+        
         def __repr__(self):
-            return '<Pet %r>' % (self.name)
-    return Pet
+            return '<Website_Analytics %r>' % (self.name)
+    return Website_Analytics
