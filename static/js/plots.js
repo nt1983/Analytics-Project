3bdd33a6bb1e8f2e7c19ac74f39ee145071c5241
walaa <w.alani2010@yahoo.com>
2021-04-14 17:46:25 -0700

    the final folder

---
+++ static/js/plots.js
@@ -0,0 +1,30 @@
+d3.csv("././static/data/countryPerDateVisits.csv").then(function(data){
+  var visits = data.map(row => row.fullVisitorId).count;
+  //var hits = visits.slice(0,5).reverse();
+  //var hits = data.sort(function(a,b) {
+  //  return +a.visits - +b.visits});
+
+  var country = data.map(row => row.country);
+// Create the Trace
+var trace = {
+  x: visits,
+  y: country,
+  type: "bar",
+  orientation: "h"
+};
+
+// Create the data array for the plot
+var data = [trace];
+
+// Define the plot layout
+var layout = {
+  title: "Web Hits per Country during Black Friday",
+  xaxis: { title: "Visits" },
+  yaxis: { title: "countries" }
+};
+
+// Plot the chart to a div tag with id "bar-plot"
+Plotly.newPlot("bar-plot", data, layout);
+
+
+});
